b877db266d6a2f06b8b3bbb514796e4c5242122d
Aarash Feizi <aarash95@gmail.com>
2021-12-30 22:26:54 -0500

    minor fix

--- dataset/base.py
+++ dataset/base.py
@@ -70,7 +70,7 @@ class BaseDataset_hdf5(torch.utils.data.Dataset):
     def nb_classes(self):
         hotels = 'hotels' in self.root
         assert (set(self.ys) == set(self.classes)) or hotels
-        return len(self.classes)
+        return len(self.ys)
 
     def __len__(self):
         return len(self.ys)
